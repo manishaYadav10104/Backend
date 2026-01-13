3567cf54d9e683da59f2ba86ed7ef88ae2308fd5
Manisha <yadavmanu10104@gmail.com>
2026-01-13 20:52:44 +0530

    problem resolve

--- Day07-mongoDB/database.js
+++ Day07-mongoDB/database.js
@@ -1,38 +1,30 @@
-
-
-
-
-
 const { MongoClient } = require('mongodb');
-// or as an es module:
-// import { MongoClient } from 'mongodb'
+const dotenv = require('dotenv');
 
-// Connection URL
-const url="mongodb+srv://ManishaYadav10104:Manisha%4010104@manisha10104.qemkidy.mongodb.net/";
+dotenv.config();
+
+// Connection URL (from .env)
+const url = process.env.MONGO_URI;
 const client = new MongoClient(url);
 
 // Database Name
 const dbName = 'coding';
 
 async function main() {
-  // Use connect method to connect to the server
   await client.connect();
-  console.log('Connected successfully to server');
+  console.log('Connected successfully to MongoDB');
+
   const db = client.db(dbName);
   const collection = db.collection('user');
 
-  // the following code examples can be pasted here...
-//   const findResult = await collection.find({}).toArray();
-// console.log('Found documents =>', findResult);
+  const insertResult = await collection.insertOne({
+    name: "saloni",
+    age: 18
+  });
 
-const insertResult = await collection.insertOne({ name: "saloni", age: 18 });
-console.log('Inserted documents =>', insertResult);
-
-
-  return 'done.';
+  console.log('Inserted document =>', insertResult.insertedId);
 }
 
 main()
-  .then(console.log)
   .catch(console.error)
   .finally(() => client.close());
